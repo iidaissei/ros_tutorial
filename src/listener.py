5fd577422e0acb6bd629a0700d9a79b6110157da
iidaissei <issei.288.higashi@icloud.com>
2020-10-01 09:12:04 +0900

    2020/10/01

--- src/listener.py
+++ src/listener.py
@@ -8,7 +8,9 @@ def callback(data):
     rospy.loginfo("Subscribed %s", data.data)
 
 def listener():
+    # ('トピック名', メッセージ型, コールバック関数)
     rospy.Subscriber('chatter', String, callback)
+    # 処理を継続するための記述
     rospy.spin()
 
 if __name__ == '__main__':
--- src/talker.py
+++ src/talker.py
@@ -5,7 +5,9 @@ import rospy
 from std_msgs.msg import String
 
 def talker():
+    # ('トピック名', メッセージの型, キューサイズ)
     pub = rospy.Publisher('chatter', String, queue_size=10)
+    # １秒間の送信回数を決める（任意）
     r = rospy.Rate(10)
     while not rospy.is_shutdown():
         str = "hello world %s" % rospy.get_time()
